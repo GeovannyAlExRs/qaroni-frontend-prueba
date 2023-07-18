7ff89d34c2d10e98426208bd833f56564c977807
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-18 14:29:44 -0500

    Imports module and mockup card header component

--- src/app/shared/layouts/card-header-generic/card-header-generic.component.ts
+++ src/app/shared/layouts/card-header-generic/card-header-generic.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card-header-generic',
   templateUrl: './card-header-generic.component.html',
   styleUrls: ['./card-header-generic.component.scss']
 })
-export class CardHeaderGenericComponent {
+export class CardHeaderGenericComponent implements OnInit {
+
+  @Input() title: string = ''
+  @Input() subtitle: string = ''
+  @Input() itemlist: string = ''
+  @Input() itemdate: string = ''
+
+  @Input() options1: Array<any> = []
+
+  @Output() callbackData: EventEmitter<any> = new EventEmitter()
+
+  searchNews: string = ''
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
--- src/app/shared/layouts/card-header-generic/card-header-generic.module.ts
+++ src/app/shared/layouts/card-header-generic/card-header-generic.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardHeaderGenericComponent } from './card-header-generic.component';
 
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     CardHeaderGenericComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+
+    MatInputModule,
+    FormsModule,
+    MatFormFieldModule,
+  ],
+  exports: [
+    CardHeaderGenericComponent
   ]
 })
 export class CardHeaderGenericModule { }
