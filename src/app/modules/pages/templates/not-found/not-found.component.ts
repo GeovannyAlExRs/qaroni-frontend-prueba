4a48064ac7ddb0b3e645a87c948a4a13227de03d
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-18 14:18:01 -0500

    Component not found

--- src/app/modules/pages/templates/not-found/not-found.component.ts
+++ src/app/modules/pages/templates/not-found/not-found.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
 
+  @Output() signOut = new EventEmitter<void>()
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  logoutUser(): void {
+    console.log('NO TE OLVIDES DE ELINMINAREL TOKEN...');
+
+    this.signOut.emit()
+  }
 }
