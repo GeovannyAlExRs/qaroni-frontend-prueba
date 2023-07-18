0ee825bd074508e69878d16dd8d2df2b06aa2596
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-18 13:47:10 -0500

    add routes path auth of app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'auth', loadChildren: () => import('@modules/pages/auth/auth.module').then(m=>m.AuthModule) },
+    ]
+  },
+  { path: '**', pathMatch:'full', redirectTo: '/404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
