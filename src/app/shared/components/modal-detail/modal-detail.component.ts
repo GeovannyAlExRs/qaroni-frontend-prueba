36ca0da43b7e12eee2d98c28cf122569d7cee6e2
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-18 14:52:53 -0500

    Import modules and mockup component Modal Detail

--- src/app/shared/components/modal-detail/modal-detail.component.ts
+++ src/app/shared/components/modal-detail/modal-detail.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-modal-detail',
   templateUrl: './modal-detail.component.html',
   styleUrls: ['./modal-detail.component.scss']
 })
-export class ModalDetailComponent {
+export class ModalDetailComponent implements OnInit{
+
+  id: string = ''
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialogRef: MatDialog) {
+    this.id = data.id
+  }
+
+  ngOnInit(): void {}
+
+  closeDialog() : void {
+    this.dialogRef.closeAll();
+  }
 
 }
--- src/app/shared/components/modal-detail/modal-detail.module.ts
+++ src/app/shared/components/modal-detail/modal-detail.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ModalDetailComponent } from './modal-detail.component';
-
-
+import { CardListModule } from '../card-list/card-list.module';
 
 @NgModule({
   declarations: [
     ModalDetailComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+
+    CardListModule
+  ],
+  exports: [
+    ModalDetailComponent
   ]
 })
 export class ModalDetailModule { }
