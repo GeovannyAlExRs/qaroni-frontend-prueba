86bdeded02b90619db301cc79e37197305bca486
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-18 14:05:36 -0500

    add routes path groups in the templates routing

--- src/app/modules/pages/templates/templates-routing.module.ts
+++ src/app/modules/pages/templates/templates-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'forms', loadChildren: () => import('@modules/pages/templates/forms/forms.module').then(m => m.FormsModule)},
+  { path: 'groups', loadChildren: () => import('@modules/pages/templates/groups/groups.module').then(m => m.GroupsModule)},
   { path: '404', loadChildren: () => import('@modules/pages/templates/not-found/not-found.module').then(m => m.NotFoundModule)},
 ];
 
