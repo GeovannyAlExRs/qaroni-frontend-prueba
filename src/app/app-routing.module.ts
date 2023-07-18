5b26d37372f4ca21b3ef89b018ffc34c32b686e8
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-18 13:56:53 -0500

    add routes path templates of app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
     path: '',
     children: [
       { path: 'auth', loadChildren: () => import('@modules/pages/auth/auth.module').then(m=>m.AuthModule) },
+      { path: '', loadChildren: () => import('@modules/pages/templates/templates.module').then(m=>m.TemplatesModule) },
     ]
   },
   { path: '**', pathMatch:'full', redirectTo: '/404' }
