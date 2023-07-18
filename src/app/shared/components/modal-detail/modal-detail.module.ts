ed6eb21258b5d7347269519e2ec3a6ad0c4b0c77
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-18 14:49:20 -0500

    Create module and component Modal Detail

---
+++ src/app/shared/components/modal-detail/modal-detail.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalDetailComponent } from './modal-detail.component';
+
+
+
+@NgModule({
+  declarations: [
+    ModalDetailComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ModalDetailModule { }
