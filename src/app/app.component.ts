f2fb298fe6dc8b96d0f7c2ea81b55790613bbc85
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-18 13:15:02 -0500

    import module header and add in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'qaroni-frontend-prueba-geovanny';
+
+  isAuthorized$!: Observable<boolean>
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+  }
+
+  logoutUser(): void {
+    this.router.navigate(['/auth/login'])
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { HeaderModule } from '@shared/layouts/header/header.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    BrowserAnimationsModule,
+    MatSidenavModule,
+
+    HeaderModule,
+    //SidebarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
