403be4ab6be692b53e5d9058941e9a2b215bd3a2
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-18 14:10:30 -0500

    add routes path news in the templates routing

--- src/app/modules/pages/templates/templates-routing.module.ts
+++ src/app/modules/pages/templates/templates-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: 'news', loadChildren: () => import('@modules/pages/templates/news/news.module').then(m => m.NewsModule)},
   { path: 'forms', loadChildren: () => import('@modules/pages/templates/forms/forms.module').then(m => m.FormsModule)},
   { path: 'groups', loadChildren: () => import('@modules/pages/templates/groups/groups.module').then(m => m.GroupsModule)},
   { path: '404', loadChildren: () => import('@modules/pages/templates/not-found/not-found.module').then(m => m.NotFoundModule)},
