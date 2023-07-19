5f1e0af46d5a9cd82dee09081ed35cb9a9d3d782
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-19 09:57:34 -0500

    add Guards canActivate in the path route Login

--- src/app/modules/pages/auth/auth-routing.module.ts
+++ src/app/modules/pages/auth/auth-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UnauthGuard } from '@core/guards/unauth/unauth.guard';
 
 const routes: Routes = [
-  { path: 'login', loadChildren: () => import('@modules/pages/auth/login/login.module').then(m => m.LoginModule) },
+  { path: 'login', loadChildren: () => import('@modules/pages/auth/login/login.module').then(m => m.LoginModule),
+    canActivate: [UnauthGuard] },
 ];
 
 @NgModule({
