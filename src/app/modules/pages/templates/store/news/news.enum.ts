7e7e9c096fa141b6f6f074ca95a35a27984720b6
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-19 10:11:31 -0500

    Create enum News

---
+++ src/app/modules/pages/templates/store/news/news.enum.ts
@@ -0,0 +1,9 @@
+export enum News {
+  READ_NEWS = '[News] Read: Start',
+  READ_NEWS_SUCCESS = '[News] Read: Success',
+  READ_NEWS_ERROR = '[News] Read: Error',
+
+  READ_NEWS_BY_ID = '[NewsID] Read: Start',
+  READ_NEWS_BY_ID_SUCCESS = '[NewsID] Read: Success',
+  READ_NEWS_BY_ID_ERROR = '[NewsID] Read: Error'
+}
