6f9cd3526e59df67fcb18e17e90989a8c070658f
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-19 17:18:30 -0500

    Create Store Reducers News

---
+++ src/app/modules/pages/templates/store/news/news.store.reducers.ts
@@ -0,0 +1,27 @@
+import { NewsResponse } from "@modules/pages/templates/store/news/news.store.interfaces";
+import * as fromActions from "@modules/pages/templates/store/news/news.store.actions";
+
+// DB Local
+export interface NewsState {
+  newslist: NewsResponse[] | null;
+  news: NewsResponse | null;
+  loading: boolean | null;
+  error: string | null;
+}
+
+const initialState: NewsState = {
+  newslist: null,
+  news: null,
+  loading: null,
+  error: null,
+}
+
+export function reducer(state: NewsState = initialState, action: fromActions.All | any) {
+  switch(action.type) {
+    case fromActions.ReadNews : return {...state, loading: true, error: null }
+    case fromActions.ReadNewsSuccess : return {...state, loading: false, newslist: action.newslist }
+    case fromActions.ReadNewsError : return {...state, loading: false, error: action.error }
+
+    default: return state
+  }
+}
