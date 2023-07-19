eb0a79a2c6ae4f61ac788259160a9670e9729c5f
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-19 09:53:00 -0500

    Create Guards UnAuth

---
+++ src/app/core/guards/unauth/unauth.guard.ts
@@ -0,0 +1,43 @@
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { Injectable } from '@angular/core';
+
+import { Store, select } from '@ngrx/store';
+import { Observable, filter, map, tap } from 'rxjs';
+
+import { getUserState } from '@store/users/users.store.selectors';
+import * as fromRoot from '@store/store'
+import { TokenService } from '@services/token/token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UnauthGuard implements CanActivate, CanActivateChild, CanLoad {
+
+  constructor(private router: Router, private store: Store<fromRoot.State>, private serviceToken: TokenService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      return this.checkSession();
+  }
+
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      return this.checkSession();
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      return this.checkSession();
+  }
+
+  private checkSession() : boolean {
+    console.log('[UNAUTH] SAVE TOKEN : ' + this.serviceToken.getToken());
+    if(this.serviceToken.getToken()) {
+      this.router.navigate(['/groups'])
+    }
+    return true;
+  }
+}
