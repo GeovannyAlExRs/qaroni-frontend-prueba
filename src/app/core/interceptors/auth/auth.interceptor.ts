be4ab049f5f71fa81e277e29807cd6e30567c03d
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-19 07:43:35 -0500

    Create interceptors for keep the user session

---
+++ src/app/core/interceptors/auth/auth.interceptor.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+
+import { TokenService } from '@services/token/token.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private serviceToken: TokenService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    try {
+      const access_token = this.serviceToken.getToken()
+
+      if(!access_token) {
+        return next.handle(request);
+      }
+
+      const newRequest = request.clone({
+        setHeaders: { authorization: `Bearer ${access_token}` }
+      })
+      console.log('TOKEN Interceptor: ', newRequest);
+      return next.handle(newRequest)
+    } catch (error) {
+      console.log('Error ', error)
+      return next.handle(request)
+    }
+  }
+}
