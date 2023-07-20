4564fdbcd34a963df3322f94697bcbfbee0803e6
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-19 20:22:27 -0500

    add effects and reducers News in templates module

--- src/app/modules/pages/templates/templates.module.ts
+++ src/app/modules/pages/templates/templates.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { TemplatesRoutingModule } from './templates-routing.module';
 
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { effects, reducers } from '@modules/pages/templates/store/data.store';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    TemplatesRoutingModule
+    TemplatesRoutingModule,
+
+    StoreModule.forFeature('news', reducers),
+    EffectsModule.forFeature(effects)
   ]
 })
 export class TemplatesModule { }
