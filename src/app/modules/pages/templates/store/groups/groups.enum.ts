0a0ca259fe54276116a114c2bf201d8ed04316c1
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-23 18:24:27 -0500

    Create enum GROUPS

---
+++ src/app/modules/pages/templates/store/groups/groups.enum.ts
@@ -0,0 +1,9 @@
+export enum News {
+  GROUPS_NEWS = '[Groups] Read: Start',
+  GROUPS_NEWS_SUCCESS = '[Groups] Read: Success',
+  READ_NEWS_ERROR = '[Groups] Read: Error',
+
+  GROUPS_NEWS_BY_ID = '[GroupsID] Read: Start',
+  GROUPS_NEWS_BY_ID_SUCCESS = '[GroupsID] Read: Success',
+  GROUPS_NEWS_BY_ID_ERROR = '[GroupsID] Read: Error'
+}
