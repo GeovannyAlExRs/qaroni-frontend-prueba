d611844518c94cd92a224378de14d3e44a3ecfd7
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-23 18:33:54 -0500

    Create store reducers GROUPS

---
+++ src/app/modules/pages/templates/store/groups/groups.store.reducers.ts
@@ -0,0 +1,27 @@
+import { GroupsResponse } from "@modules/pages/templates/store/groups/groups.store.interfaces";
+import * as fromActions from "@modules/pages/templates/store/groups/groups.store.actions";
+
+// DB Local
+export interface GroupsState {
+  groupslist: GroupsResponse[] | null;
+  groups: GroupsResponse | null;
+  loading: boolean | null;
+  error: string | null;
+}
+
+const initialState: GroupsState = {
+  groupslist: null,
+  groups: null,
+  loading: null,
+  error: null,
+}
+
+export function reducer(state: GroupsState = initialState, action: fromActions.All | any) {
+  switch(action.type) {
+    case fromActions.ReadGroups : return {...state, loading: true, error: null }
+    case fromActions.ReadGroupsSuccess : return {...state, loading: false, groupslist: action.groupslist }
+    case fromActions.ReadGroupsError : return {...state, loading: false, error: action.error }
+
+    default: return state
+  }
+}
