ec454fc02ebb59477793768cbdacf9eb12ab5a60
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-23 09:03:50 -0500

    change path forms

--- src/app/modules/pages/templates/templates-routing.module.ts
+++ src/app/modules/pages/templates/templates-routing.module.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from '@core/guards/auth/auth.guard';
 
 const routes: Routes = [
   { path: 'news', loadChildren: () => import('@modules/pages/templates/news/news.module').then(m => m.NewsModule)},
-  { path: 'forms', loadChildren: () => import('@modules/pages/templates/forms/forms.module').then(m => m.FormsModule)},
+  { path: 'forms', loadChildren: () => import('@modules/pages/templates/forms/forms.module').then(m => m.FormsParentModule)},
   { path: 'groups', loadChildren: () => import('@modules/pages/templates/groups/groups.module').then(m => m.GroupsModule),
     canActivate: [AuthGuard] },
   { path: '404', loadChildren: () => import('@modules/pages/templates/not-found/not-found.module').then(m => m.NotFoundModule)},
