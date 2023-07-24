c6ffd6397d20c8815df16c76a706013f0986f973
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-24 00:42:37 -0500

    Update model of news and groups

--- src/app/core/models/groups.model.ts
+++ src/app/core/models/groups.model.ts
@@ -1,59 +1,85 @@
 export interface Groups {
-  count:    number;
+  count: number;
   included: null;
-  input:    string;
-  result:   GroupsResult[];
-  links:    null;
+  input: string;
+  result: GroupsResult[];
+  links: null;
 }
 
 export interface GroupsResult {
-  merchantId:        number;
-  serieId:           number;
-  versionId:         null;
-  templateId:        null;
-  associationType:   AssociationType;
-  associatedGroupId: null;
-  status:            Status;
-  position:          number;
-  type:              Type;
-  isPaid:            boolean;
-  isPartner:         boolean;
-  hasApproval:       boolean;
-  hasPartner:        boolean;
-  hasPolls:          boolean;
-  name:              string;
-  description:       null | string;
-  slug:              string;
-  groupId:           number;
-  imageUrl:          null | string;
-  category:          Category | null;
-  serie:             Serie;
+  merchantId: number;
+  associatedGroupId: number | null;
+  formId: number | null;
+  categoryId: number | null;
+  emailTemplateId: number | null;
+  serieId: number | null;
+  versionId: number | null;
+  associationType: string | null;
+  status: string | null;
+  position: number | null;
+  type: string | null;
+  templateId: number | null;
+  isPaid: boolean;
+  isPartner: boolean;
+  hasPartner: boolean;
+  hasApproval: boolean;
+  language: string | null;
+  name: string | null;
+  description: string | null;
+  slug: string | null;
+  category: string | null;
+  productId: number | null;
+  variantId: number | null;
+  price: number | null;
+  iva: number | null;
+  creationDate: string | null;
+  lastUpdateDate: string | null;
+  groupId: number | null;
+  imageUrl: string | null;
+  templateUUID: number | null;
+  setting?: Setting;
+  version: string | null;
+  metatags?: Metatags[];
 }
 
-export enum AssociationType {
-  Main = "MAIN",
+export interface Setting {
+  groupId: number | null;
+  schemaId: number | null;
+  mutualityId: string | null;
+  alertDays: number | null;
+  bankCharge: number | null;
+  keepCode: number | null;
+  hasRenewal: number | null;
+  hasExpiration: number | null;
+  hasInscriptionNotification: number | null;
+  visibility: string | null;
+  notificationEmail: string | null;
+  renovationType: string | null;
+  renovationNumber: number | null;
+  feeNumber: number | null;
+  codeField: string | null;
+  creationDate: string | null;
+  lastUpdateDate: string | null;
 }
 
-export interface Category {
-  language:    string;
-  slug:        string;
-  name:        string;
-  description: null | string;
+export interface Metatags {
+  merchantId: number;
+  status: string | null;
+  name: string | null;
+  type: string | null;
+  typeId: number | null;
+  creationDate: string | null;
+  lastUpdateDate: string | null;
+  metatagId: number;
+  typeName: string | null;
+  descriptions?: Descriptions[]
 }
 
-export interface Serie {
-  preffix:        string;
-  creationDate:   string;
-  lastUpdateDate: string;
-  serieId:        number;
-}
-
-export enum Status {
-  Active = "ACTIVE",
-  Inactive = "INACTIVE",
-}
-
-export enum Type {
-  Entity = "ENTITY",
-  Person = "PERSON",
+export interface Descriptions {
+  language: string;
+  title: string | null;
+  description: string | null;
+  keywords?: string | null;
+  creationDate?: string | null;
+  lastUpdateDate?: string | null;
 }
--- src/app/core/models/news.model.ts
+++ src/app/core/models/news.model.ts
@@ -8,31 +8,48 @@ export interface News {
 
 export interface NewsResult {
   merchantId: number;
-  status: string;
-  name: null;
+  status: string | null;
+  name: string | null;
   featured: boolean;
-  date: string;
-  title: string;
-  subtitle: string;
-  slug: string;
-  shortDescription: string;
-  largeDescription: string;
-  creationDate: string;
-  lastUpdateDate: string;
-  newId: number;
-  imagesURL: string[];
-  imageUrl: string;
-  authors: any[];
-  tags: any[];
-  categories: any[];
-  descriptions: Description[];
+  date: string | null;
+  title: string | null;
+  subtitle: string | null;
+  slug: string | null;
+  shortDescription: string | null;
+  largeDescription: string | null;
+  creationDate: string | null;
+  lastUpdateDate: string | null;
+  newId: number
+  imagesURL: [string] | null;
+  imageUrl: string | null;
+  metatags?: Metatags[]
+  descriptions?: Descriptions[]
+  authors: any[] | null;
+  categories: any[] | null;
+  tags: any[] | null;
 }
 
-export interface Description {
+export interface Metatags {
+  merchantId: number;
+  status: string | null;
+  name: string | null;
+  typeId: number;
+  creationDate: string | null;
+  lastUpdateDate: string | null;
+  metatagId: number;
+  typeName: string | null;
+  descriptions?: Descriptions[]
+}
+
+export interface Descriptions {
   language: string;
-  slug: string;
-  title: string;
-  subtitle: string;
-  shortDescription: string;
-  largeDescription: string;
+  slug?: string | null;
+  title: string | null;
+  subtitle?: string | null;
+  shortDescription?: string | null;
+  largeDescription?: string | null;
+  description?: string | null;
+  keywords?: string | null;
+  creationDate?: string | null;
+  lastUpdateDate?: string | null;
 }
