09452999dd4d1953f3a95c90facd89a956b56937
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-24 13:44:26 -0500

    Testing spect.ts of component FORM PERSON

--- src/app/shared/components/form/form-person/form-person.component.spec.ts
+++ src/app/shared/components/form/form-person/form-person.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormPersonComponent } from './form-person.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 describe('FormPersonComponent', () => {
   let component: FormPersonComponent;
@@ -8,6 +11,13 @@ describe('FormPersonComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+      ],
       declarations: [FormPersonComponent]
     });
     fixture = TestBed.createComponent(FormPersonComponent);
