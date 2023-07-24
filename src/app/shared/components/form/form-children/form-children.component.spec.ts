e512747db085a2d11862c4f0e7855e1a22562abc
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-24 13:46:29 -0500

    Testing spect.ts of component FORM CHILDREN

--- src/app/shared/components/form/form-children/form-children.component.spec.ts
+++ src/app/shared/components/form/form-children/form-children.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormChildrenComponent } from './form-children.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 describe('FormChildrenComponent', () => {
   let component: FormChildrenComponent;
@@ -8,6 +11,13 @@ describe('FormChildrenComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+      ],
       declarations: [FormChildrenComponent]
     });
     fixture = TestBed.createComponent(FormChildrenComponent);
