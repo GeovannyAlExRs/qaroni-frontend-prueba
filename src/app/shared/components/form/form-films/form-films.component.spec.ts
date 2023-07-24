1a8e59858d62f5ba1d338d0849fecdb98cc1b899
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-24 13:46:10 -0500

    Testing spect.ts of component FORM FILMS

--- src/app/shared/components/form/form-films/form-films.component.spec.ts
+++ src/app/shared/components/form/form-films/form-films.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormFilmsComponent } from './form-films.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 describe('FormFilmsComponent', () => {
   let component: FormFilmsComponent;
@@ -8,6 +11,13 @@ describe('FormFilmsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+      ],
       declarations: [FormFilmsComponent]
     });
     fixture = TestBed.createComponent(FormFilmsComponent);
