05bd3cebe10b309457a814cd8aeb2ed346351622
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-24 13:47:02 -0500

    Testing spect.ts of component NEWS

--- src/app/modules/pages/templates/news/news.component.spec.ts
+++ src/app/modules/pages/templates/news/news.component.spec.ts
@@ -1,6 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewsComponent } from './news.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+import { effects, reducers } from '@store/store';
+import { effects as effectsN, reducers as reducersN } from '@modules/pages/templates/store/data.store';
+
+import { CardHeaderGenericModule } from '@shared/layouts/card-header-generic/card-header-generic.module';
+import { CardListModule } from '@shared/components/card-list/card-list.module';
 
 describe('NewsComponent', () => {
   let component: NewsComponent;
@@ -8,6 +19,21 @@ describe('NewsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+
+        CardHeaderGenericModule,
+        CardListModule,
+
+        StoreModule.forRoot(reducers, {
+          runtimeChecks: { strictActionImmutability: true, strictStateImmutability: true }
+        }),
+        EffectsModule.forRoot(effects),
+
+        StoreModule.forFeature('news', reducersN),
+        EffectsModule.forFeature(effectsN)
+      ],
       declarations: [NewsComponent]
     });
     fixture = TestBed.createComponent(NewsComponent);
