2d35a13346336157f87a3cbf6dcff541634f498a
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-24 14:07:17 -0500

    Testing spect Guards

--- src/app/core/guards/auth/auth.guard.spec.ts
+++ src/app/core/guards/auth/auth.guard.spec.ts
@@ -2,13 +2,26 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { StoreModule } from '@ngrx/store';
+import { effects, reducers } from '@store/store';
+import { EffectsModule } from '@ngrx/effects';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule]
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+
+        StoreModule.forRoot(reducers, {
+          runtimeChecks: { strictActionImmutability: true, strictStateImmutability: true }
+        }),
+        EffectsModule.forRoot(effects),
+      ]
     });
     guard = TestBed.inject(AuthGuard);
   });
--- src/app/core/guards/unauth/unauth.guard.spec.ts
+++ src/app/core/guards/unauth/unauth.guard.spec.ts
@@ -2,13 +2,27 @@ import { TestBed } from '@angular/core/testing';
 
 import { UnauthGuard } from './unauth.guard';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { StoreModule } from '@ngrx/store';
+import { effects, reducers } from '@store/store';
+import { EffectsModule } from '@ngrx/effects';
+
 
 describe('UnauthGuard', () => {
   let guard: UnauthGuard;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule]
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+
+        StoreModule.forRoot(reducers, {
+          runtimeChecks: { strictActionImmutability: true, strictStateImmutability: true }
+        }),
+        EffectsModule.forRoot(effects),
+      ]
     });
     guard = TestBed.inject(UnauthGuard);
   });
