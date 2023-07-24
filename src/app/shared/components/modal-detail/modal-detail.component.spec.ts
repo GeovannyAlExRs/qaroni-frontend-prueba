843b263be4c7699cbed1f29ce477e99a62835496
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-24 17:17:25 -0500

    Testing component MODAL DETAIL

--- src/app/shared/components/modal-detail/modal-detail.component.spec.ts
+++ src/app/shared/components/modal-detail/modal-detail.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModalDetailComponent } from './modal-detail.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 
 describe('ModalDetailComponent', () => {
   let component: ModalDetailComponent;
@@ -8,6 +10,13 @@ describe('ModalDetailComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      providers: [{provide: MAT_DIALOG_DATA, useValue: {}}],
+      imports: [
+        //BrowserAnimationsModule,
+        RouterTestingModule,
+        MatDialogModule,
+
+      ],
       declarations: [ModalDetailComponent]
     });
     fixture = TestBed.createComponent(ModalDetailComponent);
