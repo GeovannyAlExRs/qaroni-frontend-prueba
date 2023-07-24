de76816aaa9568066e0e60be42880deb70e8818b
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-24 13:46:44 -0500

    Testing spect.ts of component CARD LIST

--- src/app/shared/components/card-list/card-list.component.spec.ts
+++ src/app/shared/components/card-list/card-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardListComponent } from './card-list.component';
+import { MatDialog } from '@angular/material/dialog';
 
 describe('CardListComponent', () => {
   let component: CardListComponent;
@@ -8,6 +9,7 @@ describe('CardListComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      providers: [MatDialog],
       declarations: [CardListComponent]
     });
     fixture = TestBed.createComponent(CardListComponent);
