18bb6feaa73fda9b0499d14bab84061f0ee64086
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-24 13:47:59 -0500

    Testing spect.ts of component LOGIN

--- src/app/modules/pages/auth/login/login.component.spec.ts
+++ src/app/modules/pages/auth/login/login.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { StoreModule } from '@ngrx/store';
+import { effects, reducers } from '@store/store';
+import { EffectsModule } from '@ngrx/effects';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +18,21 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+
+        ReactiveFormsModule,
+
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+
+        StoreModule.forRoot(reducers, {
+          runtimeChecks: { strictActionImmutability: true, strictStateImmutability: true }
+        }),
+        EffectsModule.forRoot(effects),
+      ],
       declarations: [LoginComponent]
     });
     fixture = TestBed.createComponent(LoginComponent);
