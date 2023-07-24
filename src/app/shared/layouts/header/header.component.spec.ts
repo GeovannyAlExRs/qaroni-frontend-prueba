9d948a5b8c5ae64c0b20d8a6f8c81ba0a53feacf
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-24 13:44:00 -0500

    Testing spect.ts of component HEADER

--- src/app/shared/layouts/header/header.component.spec.ts
+++ src/app/shared/layouts/header/header.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,6 +14,14 @@ describe('HeaderComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule
+      ],
       declarations: [HeaderComponent]
     });
     fixture = TestBed.createComponent(HeaderComponent);
