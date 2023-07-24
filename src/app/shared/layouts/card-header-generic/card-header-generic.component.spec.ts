83029ef7f8866cdcdbedec44eb9172b2763146f3
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-24 13:44:13 -0500

    Testing spect.ts of component CARD HEADER

--- src/app/shared/layouts/card-header-generic/card-header-generic.component.spec.ts
+++ src/app/shared/layouts/card-header-generic/card-header-generic.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardHeaderGenericComponent } from './card-header-generic.component';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 describe('CardHeaderGenericComponent', () => {
   let component: CardHeaderGenericComponent;
@@ -8,6 +11,11 @@ describe('CardHeaderGenericComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MatInputModule,
+        FormsModule,
+        MatFormFieldModule,
+      ],
       declarations: [CardHeaderGenericComponent]
     });
     fixture = TestBed.createComponent(CardHeaderGenericComponent);
