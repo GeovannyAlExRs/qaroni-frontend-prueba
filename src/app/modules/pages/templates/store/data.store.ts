6bdecee88e377d80ed7bb45ff50af4818a2132ac
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-24 00:45:11 -0500

    Add STORE GROUPS in the data

--- src/app/modules/pages/templates/store/data.store.ts
+++ src/app/modules/pages/templates/store/data.store.ts
@@ -3,7 +3,10 @@ import { ActionReducerMap, createFeatureSelector } from "@ngrx/store";
 import { NewsState, reducer } from "@modules/pages/templates/store/news/news.store.reducers";
 import { NewsEffects } from "@modules/pages/templates/store/news/news.store.effects";
 
+import { GroupsState, reducer as reducerG} from "@modules/pages/templates/store/groups/groups.store.reducers";
+import { GroupsEffects } from "@modules/pages/templates/store/groups/groups.store.effects";
 
+// NEWS
 export interface NewsListState { list: NewsState }
 
 export const getNewsState = createFeatureSelector<NewsListState>('news')
@@ -11,3 +14,12 @@ export const getNewsState = createFeatureSelector<NewsListState>('news')
 export const reducers  : ActionReducerMap<NewsListState> = { list: reducer }
 
 export const effects : any = [ NewsEffects ]
+
+//GROUPS
+export interface GroupsListState { list: GroupsState }
+
+export const getGroupsState = createFeatureSelector<GroupsListState>('groups')
+
+export const reducerGroup  : ActionReducerMap<GroupsListState> = { list: reducerG }
+
+export const effectsGroup : any = [ GroupsEffects ]
