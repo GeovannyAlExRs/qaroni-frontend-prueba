6f04d018dc761592db2f36daf3ce2514115fbd3f
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-07-24 13:47:17 -0500

    Testing spect.ts of component GROUPS

--- src/app/modules/pages/templates/groups/groups.component.spec.ts
+++ src/app/modules/pages/templates/groups/groups.component.spec.ts
@@ -1,6 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GroupsComponent } from './groups.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+import { effects, reducers } from '@store/store';
+import { effectsGroup, reducerGroup } from '@modules/pages/templates/store/data.store';
+
+import { CardHeaderGenericModule } from '@shared/layouts/card-header-generic/card-header-generic.module';
+import { CardListModule } from '@shared/components/card-list/card-list.module';
 
 describe('GroupsComponent', () => {
   let component: GroupsComponent;
@@ -8,6 +19,21 @@ describe('GroupsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+
+        CardHeaderGenericModule,
+        CardListModule,
+
+        StoreModule.forRoot(reducers, {
+          runtimeChecks: { strictActionImmutability: true, strictStateImmutability: true }
+        }),
+        EffectsModule.forRoot(effects),
+
+        StoreModule.forFeature('news', reducerGroup),
+        EffectsModule.forFeature(effectsGroup)
+      ],
       declarations: [GroupsComponent]
     });
     fixture = TestBed.createComponent(GroupsComponent);
